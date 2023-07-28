676982fb2930bb3dcc4d78df2b797296c8145dfb
Hannah <hannahrwatkins@gmail.com>
2023-07-28 09:36:04 -0400

    added routes to server.js for authentiation and verse search

--- server.js
+++ server.js
@@ -4,31 +4,76 @@ const serviceAccount = require("/Users/Hannah/ada/capstone/backend_bible_app/bib
 const axios = require("axios");
 
 
-// Initialize express app
+// initialize express app
 const app = express();
 
-// Initialize Firebase Admin SDK
+// initialize Firebase Admin SDK
 admin.initializeApp({
 credential: admin.credential.cert(serviceAccount),
 databaseURL: "https://bible-app-f6a3f-default-rtdb.firebaseio.com/"
 });
 
 // Define routes
-app.get("/generate", (req, res) => {
-  // Handle generating new Bible verses based on keyword/topic input
-  // Retrieve relevant verses from the Bible API and save them to the database
-  // Return the generated verses as a response
+
+app.get("/generate", async (req, res) => {
+    try {
+      // extract keyword or topic from query parameters
+        const { keyword, topic } = req.query;
+
+      // validate that either keyword or topic is provided
+        if (!keyword && !topic) {
+        return res.status(400).json({ error: "Please provide a keyword or topic." });
+    }
+
+    //defining api url for getting Bible verses
+    const apiUrl = "https://scripture.api.bible/admin";
+
+
+    //GET request to Bible api w/ keyword/topic as query peram
+        const response = await axios.get(apiUrl, {
+        params: {
+            keyword: keyword,
+            topic: topic,
+        },
+    });
+
+    const verses = response.data;
+
+    // Save fetched verses to Firebase Realtime Database
+    const databaseRef = admin.database().ref("verses");
+    verses.forEach((verse) => {
+        databaseRef.push(verse);
+    });
+
+    // Return generated verses as response
+    res.json(verses);
+    } catch (error) {
+    console.error("Error generating Bible verses:", error);
+    res.status(500).json({ error: "An error occurred while generating Bible verses." });
+    }
 });
 
-app.get("/verses", (req, res) => {
-  // Handle retrieving saved Bible verses for a specific user
-  // Return the saved verses as a response
-});
-
-
-// Start the server
+// start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 console.log(`Server is running on port ${port}`);
 });
+
+//verses route
+app.get("/verses", async (req, res) => {
+    //retrieving saved veres for specific user from Realtime DB 
+    try {
+        const userId = req.user.id;
+        const databaseRef = admin.database().ref(`users/${userId}/verses`);
+        const snapshot = await databaseRef.once("value");
+        const savedVerses = snapshot.val();
+
+        // return saved verses as response
+        res.json(savedVerses);
+    } catch (error) {
+        console.error("Error retrieving saved Bible verses:", error);
+        res.status(500).json({ error: "An error occurred while retrieving saved Bible verses." });
+    }
+});
+
 
